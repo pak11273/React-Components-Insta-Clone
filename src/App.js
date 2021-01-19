85e6218c4ffd2b9297d37dcc1ff09bd15eb90dad
pak11273 <pak11273@gmail.com>
2021-01-19 16:02:20 -0600

    adds search functionality

--- src/App.js
+++ src/App.js
@@ -6,13 +6,13 @@
 
 // Import the state hook
 
-import './App.css';
+import "./App.css";
 
-import React, {useState} from 'react';
+import React, { useEffect, useState } from "react";
 
-import Posts from './components/Posts/Posts'
-import SearchBar from './components/SearchBar/SearchBar.js'
-import dummyData from './dummy-data'
+import Posts from "./components/Posts/Posts";
+import SearchBar from "./components/SearchBar/SearchBar.js";
+import dummyData from "./dummy-data";
 
 // Import the Posts (plural!) and SearchBar components, since they are used inside App component
 // Import the dummyData
@@ -21,10 +21,27 @@ const App = () => {
   // Create a state called `posts` to hold the array of post objects, **initializing to dummyData**.
   // This state is the source of truth for the data inside the app. You won't be needing dummyData anymore.
   // To make the search bar work (which is stretch) we'd need another state to hold the search term.
-  const [posts, setPosts] = useState(dummyData)
-  // const [searchTerm, setSearch] = useState("")
+  const [posts, setPosts] = useState(dummyData);
+  const [searchTerm, setSearch] = useState("");
+  useEffect(() => {
+    searchByUsername();
+  }, [searchTerm]);
 
-  const likePost = postId => {
+  const inputChange = (e) => {
+    setSearch(e.target.value);
+  };
+
+  const searchByUsername = () => {
+    let newPosts = [];
+    posts.forEach((post) => {
+      if (post.username.includes(searchTerm)) {
+        newPosts.push(post);
+        setPosts(newPosts);
+      }
+    });
+  };
+
+  const likePost = (postId) => {
     /*
       This function serves the purpose of increasing the number of likes by one, of the post with a given id.
 
@@ -36,21 +53,21 @@ const App = () => {
         - if the `id` of the post matches `postId`, return a new post object with the desired values (use the spread operator).
         - otherwise just return the post object unchanged.
      */
-      let newPosts = posts.map( post => {
-        if(post.id === postId) {
-          post.likes++
-        }
-        return post
-      })
+    let newPosts = posts.map((post) => {
+      if (post.id === postId) {
+        post.likes++;
+      }
+      return post;
+    });
 
-      setPosts(newPosts)
+    setPosts(newPosts);
   };
 
   return (
-    <div className='App'>
+    <div className="App">
       {/* Add SearchBar and Posts here to render them */}
-      <SearchBar />
-      <Posts likePost={likePost} posts={posts}/>
+      <SearchBar searchTerm={searchTerm} inputChange={inputChange} />
+      <Posts likePost={likePost} posts={posts} />
       {/* Check the implementation of each component, to see what props they require, if any! */}
     </div>
   );
--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -1,21 +1,25 @@
 // You do not need to change any code in this file for MVP
-import React from "react";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faInstagram } from '@fortawesome/free-brands-svg-icons';
-import { faHeart, faCircle, faCompass } from '@fortawesome/free-regular-svg-icons';
+
 import "./SearchBar.css";
 
-const SearchBar = (props) => {
+import {
+  faCircle,
+  faCompass,
+  faHeart,
+} from "@fortawesome/free-regular-svg-icons";
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import React from "react";
+import { faInstagram } from "@fortawesome/free-brands-svg-icons";
+
+const SearchBar = ({ searchTerm, inputChange }) => {
   return (
     <div className="search-bar-wrapper">
       <div className="social">
         <FontAwesomeIcon icon={faInstagram} />
       </div>
       <form className="search-form">
-        <input
-          type="text"
-          placeholder="Search"
-        />
+        <input type="text" placeholder="Search" onChange={inputChange} />
       </form>
       <div className="social-wrapper">
         <div className="social">
